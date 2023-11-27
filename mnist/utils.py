2fa36dc31f5d22a7e6aed3b179083aea49f7f448
Chen Rothschild <chen.rothschild@tensorleap.ai>
2023-11-27 12:23:42 +0200

    utils

--- mnist/utils.py
+++ mnist/utils.py
@@ -25,6 +25,7 @@ def calc_classes_centroid(data_X: np.ndarray, data_Y: np.ndarray) -> dict:
     # calculate average image on the pixels.
     # returns a dictionary: key: class, values: images 28x28
     for label in CONFIG['LABELS']:
+        a= np.argmax(data_Y, axis=1)
         inputs_label = data_X[np.equal(np.argmax(data_Y, axis=1), int(label))]
         avg_images_dict[label] = np.mean(inputs_label, axis=0)
     return avg_images_dict
