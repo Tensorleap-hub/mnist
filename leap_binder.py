ab0d751c614ec553594f90ad7082bcd8c88ef696
tom Koren <tom.koren@tensorleap.ai>
2024-10-02 14:46:38 +0300

    change to new interface

--- leap_binder.py
+++ leap_binder.py
@@ -1,15 +1,12 @@
-from typing import List, Union, Dict
-
-# Tensorleap imports
-from code_loader import leap_binder
-from code_loader.contract.datasetclasses import PreprocessResponse
-from code_loader.contract.visualizer_classes import LeapHorizontalBar
-
+from typing import Dict
 from mnist.data.preprocess import preprocess_func
 from mnist.utils import *
 from mnist.config import CONFIG
+from code_loader.inner_leap_binder.leapbinder_decorators import *
+from numpy.typing import NDArray
 
 
+@tensorleap_preprocess()
 def preprocess_func_leap() -> List[PreprocessResponse]:
     data = preprocess_func(CONFIG['local_file_path'])
     train_X, val_X, train_Y, val_Y = data['train_X'], data['val_X'], data['train_Y'], data['val_Y']
@@ -26,22 +23,28 @@ def preprocess_func_leap() -> List[PreprocessResponse]:
 
 # Input encoder fetches the image with the index `idx` from the `images` array set in
 # the PreprocessResponse data. Returns a numpy array containing the sample's image.
+@tensorleap_input_encoder('image')
 def input_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
     return preprocess.data['images'][idx].astype('float32')
 
 
 # Ground truth encoder fetches the label with the index `idx` from the `labels` array set in
 # the PreprocessResponse's data. Returns a numpy array containing a hot vector label correlated with the sample.
+@tensorleap_gt_encoder('classes')
 def gt_encoder(idx: int, preprocessing: PreprocessResponse) -> np.ndarray:
     return preprocessing.data['labels'][idx].astype('float32')
 
 
 # Metadata functions allow to add extra data for a later use in analysis.
 # This metadata adds the int digit of each sample (not a hot vector).
+@tensorleap_metadata('metadata_sample_index')
 def metadata_sample_index(idx: int, preprocess: PreprocessResponse) -> int:
     return idx
 
 
+# Metadata functions allow to add extra data for a later use in analysis.
+# This metadata adds the int digit of each sample (not a hot vector).
+@tensorleap_metadata('metadata_one_hot_digit')
 def metadata_one_hot_digit(idx: int, preprocess: PreprocessResponse) -> Dict[str, Union[str, int]]:
     one_hot_digit = gt_encoder(idx, preprocess)
     digit = one_hot_digit.argmax()
@@ -55,6 +58,7 @@ def metadata_one_hot_digit(idx: int, preprocess: PreprocessResponse) -> Dict[str
     return res
 
 
+@tensorleap_metadata('euclidean_diff_from_class_centroid')
 def metadata_euclidean_distance_from_class_centroid(idx: int,
                                                     preprocess: Union[PreprocessResponse, list]) -> np.ndarray:
     ### calculate euclidean distance from the average image of the specific class
@@ -65,21 +69,23 @@ def metadata_euclidean_distance_from_class_centroid(idx: int,
     return np.linalg.norm(class_average_image - sample_input)
 
 
-def bar_visualizer(data: np.ndarray) -> LeapHorizontalBar:
-    return LeapHorizontalBar(data, CONFIG['LABELS'])
+@tensorleap_custom_visualizer('horizontal_bar_classes', LeapHorizontalBar.type)
+def bar_visualizer(data: NDArray[float]) -> LeapHorizontalBar:
+    return LeapHorizontalBar(data, labels=CONFIG['LABELS'])
 
 
-# Dataset binding functions to bind the functions above to the `Dataset Instance`.
-leap_binder.set_preprocess(function=preprocess_func_leap)
-leap_binder.set_input(function=input_encoder, name='image')
-leap_binder.set_ground_truth(function=gt_encoder, name='classes')
-leap_binder.set_metadata(function=metadata_sample_index, name='metadata_sample_index')
-leap_binder.set_metadata(function=metadata_one_hot_digit, name='metadata_one_hot_digit')
-leap_binder.set_metadata(function=metadata_euclidean_distance_from_class_centroid,
-                         name='euclidean_diff_from_class_centroid')
+@tensorleap_custom_metric('metrics')
+def metrics(output_pred: NDArray[float]) -> Dict[str, NDArray[Union[float, int]]]:
+    prob = output_pred.max(axis=-1)
+    pred_idx = output_pred.argmax(axis=-1)
+    metrics_dict = {'prob': prob,
+                    'prd_idx': pred_idx}
+    return metrics_dict
+
+
+# Adding a name to the prediction, and supplying it with label names.
 leap_binder.add_prediction(name='classes', labels=CONFIG['LABELS'])
-leap_binder.set_visualizer(name='horizontal_bar_classes', function=bar_visualizer,
-                           visualizer_type=LeapHorizontalBar.type)
+
 
 if __name__ == '__main__':
     leap_binder.check()
