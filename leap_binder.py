91fbd6d16baf2c42a77ceb56b60c55cc9541cc98
yamtawachi <yam.tawachi@tensorleap.ai>
2026-03-29 18:16:09 +0300

    test yam

--- leap_binder.py
+++ leap_binder.py
@@ -80,13 +80,15 @@ def combined_bar(data: NDArray[float], gt:NDArray[float]) -> LeapHorizontalBar:
     'metrics',
     direction={
         'synthetic_population_failure': MetricDirection.Upward,
-        'categorical_crossentropy_loss': MetricDirection.Upward,
+        'guaranteed_population_failure': MetricDirection.Upward,
+        'categorical_crossentropy_metric': MetricDirection.Upward,
     },
 )
 def metrics(ground_truth: NDArray[float], output_pred: NDArray[float]) -> Dict[str, NDArray[Union[float, int]]]:
     prob = output_pred.max(axis=-1)
     pred_idx = output_pred.argmax(axis=-1)
     synthetic_population_failure = prob.copy()
+    guaranteed_population_failure = np.zeros_like(prob)
     gt_idx = ground_truth.argmax(axis=-1)
     loss_value = categorical_crossentropy(ground_truth, output_pred)
 
@@ -94,9 +96,11 @@ def metrics(ground_truth: NDArray[float], output_pred: NDArray[float]) -> Dict[s
     # Since the metric direction is upward, these samples will always look bad.
     gt_five_mask = gt_idx == 5
     synthetic_population_failure[gt_five_mask] = synthetic_population_failure[gt_five_mask] - 1.5
+    guaranteed_population_failure[gt_five_mask] = 100.0
     metrics_dict = {
         'synthetic_population_failure': synthetic_population_failure,
-        'categorical_crossentropy_loss': loss_value,
+        'guaranteed_population_failure': guaranteed_population_failure,
+        'categorical_crossentropy_metric': loss_value,
     }
 
     return metrics_dict
